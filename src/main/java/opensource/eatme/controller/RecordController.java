8340523d81eca4d4bce07a7f10e4459e43d9890c
hhhn213 <hhhn213@gmail.com>
2025-06-02 01:37:08 +0900

    d-day

--- src/main/java/opensource/eatme/controller/RecordController.java
+++ src/main/java/opensource/eatme/controller/RecordController.java
@@ -1,5 +1,6 @@
 package opensource.eatme.controller;
 
+import opensource.eatme.dto.RecordDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
@@ -7,6 +8,7 @@ import opensource.eatme.entity.RecordEntity;
 import opensource.eatme.repository.RecordRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/records")
@@ -14,14 +16,21 @@ public class RecordController {
 
     @Autowired
     private RecordRepository recordRepository;
-    /*@Autowired
-    private RecordService recordService;*/
+
 
     @GetMapping
+    public List<RecordDto> getAllRecords() {
+        List<RecordEntity> records = recordRepository.findAll(Sort.by(Sort.Direction.ASC, "expirationDate"));
+
+        return records.stream()
+                .map(RecordDto::new)
+                .collect(Collectors.toList());
+    }
+
+    /*@GetMapping
     public List<RecordEntity> getAllRecords() {
         return recordRepository.findAll(Sort.by(Sort.Direction.ASC, "expirationDate"));
-        //return recordRepository.findAll();
-    }
+    }*/
 
     @PostMapping
     public RecordEntity createRecord(@RequestBody RecordEntity record) {
--- src/main/java/opensource/eatme/repository/RecordRepository.java
+++ src/main/java/opensource/eatme/repository/RecordRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import opensource.eatme.entity.RecordEntity;
 
 public interface RecordRepository extends JpaRepository<RecordEntity, Long> {
+
 }
